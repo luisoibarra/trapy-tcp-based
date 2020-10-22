0d92a9d13d34cc358eb574ee8ad0b83423ceb405
Luiso <luise98cu@gamil.com>
2020-10-21 21:37:02 -0400

    Bug en el proyecto

--- trapy/example_raw_recv.py
+++ trapy/example_raw_recv.py
@@ -1,6 +1,6 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
 s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
