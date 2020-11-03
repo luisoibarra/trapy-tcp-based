6c72cf14d3b7cd6f4cc474aa4da2076fc176ffe3
Luiso <luise98cu@gamil.com>
2020-11-02 20:10:15 -0500

    Fixing problems and documenting trapy

--- trapy/example_raw_recv.py
+++ trapy/example_raw_recv.py
@@ -4,7 +4,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
 s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-s.bind(('10.0.0.2', 0))
+s.bind(('127.0.0.2', 0))
 
 while True:
     print(s.recvfrom(65565))
--- trapy/trapy.py
+++ trapy/trapy.py
@@ -1,30 +1,54 @@
 class Conn:
+    """
+    Base Conn class
+    """
     pass
 
 
 class ConnException(Exception):
+    """
+    Base Connection Exception
+    """
     pass
 
 
 def listen(address: str) -> Conn:
+    """
+    prepara una conexión que acepta los paquetes enviados a `address`.
+    """
     pass
 
 
-def accept(conn) -> Conn:
+def accept(conn: Conn) -> Conn:
+    """
+    espera por alguna petición de conexión utilizando un `conn` creado previamente con `listen`.
+    """
     pass
 
 
 def dial(address) -> Conn:
+    """
+    establece conexión con el otro extremo indicado en `address` y devuelve la conexión.
+    """
     pass
 
 
 def send(conn: Conn, data: bytes) -> int:
+    """
+    envía los datos por la conexión y devuelve la cantidad de bytes enviados.
+    """
     pass
 
 
 def recv(conn: Conn, length: int) -> bytes:
+    """
+    bytes recibe a lo sumo length bytes almacenados en el buffer de la conexión.
+    """
     pass
 
 
 def close(conn: Conn):
+    """
+    termina la conexión.
+    """
     pass
